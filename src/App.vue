b0cc746ae0a95614aa8be1acabf0f895a731bc5b
Aadne <aadne1999@live.no>
2022-07-27 03:19:15 +0200

    full overhaul of page. Now with responsive design

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,12 @@
 
 </body>
 <template>
-<div id="container">
-  <img id="cover" src='./assets/placeholder2.jpg'>
-  <h1 @click="rand" id="info_text">Young hobby developer and student, this is my portfolio website.</h1>
-  
-</div>
+ <div class="wrapper">
+  <h1 class="about">About me</h1>
+  <h1 class="projects">Projects</h1>
+  <h1 class="main">Student and hobby developer. <br>This is my webpage.</h1>
+ </div>
     
-  
 </template>
 
 <script>
@@ -36,39 +35,24 @@ export default {
 </script>
 
 <style>
-#container #info_text {
-  position: relative;
-  top: -230px;
-  right: -160px;
-  width: 240px;
+.wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
 
 }
-#container #cover {
-  width: 150px;
-  margin-right: 0%;
-  margin-top: 0%;
-  position: relative;
-  right: 31.5%;
-
-}
-
-#container {
-  margin-left: 25%;
-  margin-top: 13%;
-  width: 400px;
-  height: 272px;
+.main {
+  font-size: 300%;
+  background: black;
+  margin-top: 30%;
+  margin-right: 30%;
+  color: white;
+  font-family: Avenir;
+  border-color: grey;
   border-style: solid;
-  border-color: rgb(58, 61, 61);
-  border-width: 5px;
-  background-color: black;
-  color: azure;
-  font-family: "Ibarra+Real+Nova", serif;
-  font-size: small;
-  text-align: center;
-  padding: 0%;
-  
-  
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -79,11 +63,26 @@ export default {
   margin-right: 0%;
   height: 670px;
   max-width: 100%;
-  background-image: url(./assets/notowned.jpg);
+  background-color: whitesmoke;
   border-style: solid;
   border-width: 3px;
   border-color: rgb(36, 35, 37);
+  position: relative;
 }
+@media all and (min-width: 620px) {
+  .main {
+    margin-top: 20%;
+    margin-right: 20%;
+    min-height: 40%;
 
+  }
+}
+@media all and (max-width: 400px) {
+  .main {
+    margin-top: 20%;
+    min-height: 40%;
+    border-color: white;
+  }
+}
 
 </style>
